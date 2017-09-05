360b77e106cb673c4b3429f0b474cd7e372c29b1
rishabjaincs <31016370+rishabjaincs@users.noreply.github.com>
2017-09-05 19:28:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 // change the text of main-text div
 var element = document.getElementById('main-text');
 element.innerHTML='New Value';
+var image = document.getElementById('madi');
+image.onclick = function(){
+    image.style.marginLeft = '100px';
+};
