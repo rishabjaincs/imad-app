35f6771a743f8c00de4ba0e7d22f93e82646be2d
rishabjaincs <31016370+rishabjaincs@users.noreply.github.com>
2017-09-05 19:36:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML='New Value';
 var image = document.getElementById('madi');
 
 var marginLeft=0;
-function marginRight(){
+function moveRight(){
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + "px";
 }
